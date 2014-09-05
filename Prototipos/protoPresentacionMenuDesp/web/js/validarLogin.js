3c6432a644232ca8c9f69e15ecbc0899382edac3
SantiagoN <saeros.560@gmail.com>
2014-09-05 12:43:25 -0300

    Separando prototipos

---
+++ Prototipos/protoPresentacionMenuDesp/web/js/validarLogin.js
@@ -0,0 +1,33 @@
+var exprNombre = /^[a-zA-Z0-9\.]*$/;
+
+$(document).ready(function () { //Verificamos que el documento este listo
+    
+     $("#botonLogin").click(function (){
+        var nombre = $("#idUsuario").val();
+
+        $("#errorNombreCaracter").fadeOut();  
+        $("#errorNombreVacio").fadeOut();
+
+
+        if(nombre === "Nombre de usuario" ) {
+            $("#errorNombreCaracter").fadeOut();
+            $("#errorNombreVacio").fadeIn("slow"); //Muestra mensaje de error
+            return false; 
+        }
+        else if(!exprNombre.test(nombre)){
+            $("#errorNombreVacio").fadeOut();
+            $("#errorNombreCaracter").fadeIn("slow"); //Muestra mensaje de error
+            return false;                             // con false sale de la secuencia
+        }
+    });//fin onClick
+    
+    
+    //Ocultando Datos luego del primer envio si son correctos
+    $("#idUsuario").keyup(function(){
+        if( $(this).val() !== "" && exprNombre.test($(this).val())){
+            $("#errorNombreCaracter, #errorNombreVacio").fadeOut();
+            return false;
+        }
+    });
+    
+});//fin ready

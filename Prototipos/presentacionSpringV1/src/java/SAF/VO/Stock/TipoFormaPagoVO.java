fbed687266cdf37fec9f53cced07553e40f1475b
fertoledo <ferdy245@hotmail.com>
2014-09-20 16:49:19 -0300

    VO facturación

---
+++ Prototipos/presentacionSpringV1/src/java/SAF/VO/Stock/TipoFormaPagoVO.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package SAF.VO.Stock;
+
+/**
+ *
+ * @author Fernanda
+ */
+public class TipoFormaPagoVO {
+    private short idTipoFormaPago;
+     private String nombre;
+
+    public TipoFormaPagoVO() {
+    }
+
+    public short getIdTipoFormaPago() {
+        return idTipoFormaPago;
+    }
+
+    public void setIdTipoFormaPago(short idTipoFormaPago) {
+        this.idTipoFormaPago = idTipoFormaPago;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+     
+     
+}

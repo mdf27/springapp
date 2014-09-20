1586fb86a3a241b3f701e588366fa9b4e7e2fa70
Majo <majo_rabaza@hotmail.com>
2014-09-19 21:38:52 -0300

    VO - Buscar ProductoProveedorVO.java

--- Prototipos/presentacionSpringV1/src/java/SAF/VO/Stock/ProductoVO.java
+++ Prototipos/presentacionSpringV1/src/java/SAF/VO/Stock/ProductoVO.java
@@ -7,6 +7,7 @@ package SAF.VO.Stock;
 
 import java.math.BigInteger;
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 
 /**
  *
@@ -15,11 +16,67 @@ import java.text.DecimalFormat;
 public class ProductoVO {
     String descripcion;
     DecimalFormat precioCompra;
-    /*
-    precioCompra
-    precioVenta
-    */
+    DecimalFormat precioVenta;
     int idTipoIva;
     boolean habilitado;
     BigInteger idTransaccion;
+
+    public ProductoVO() {
+        DecimalFormatSymbols simbolos = new DecimalFormatSymbols();
+        simbolos.setDecimalSeparator('.');
+        precioCompra = new DecimalFormat("#####.##",simbolos);
+        precioVenta = new DecimalFormat("#####.##",simbolos);        
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public DecimalFormat getPrecioCompra() {
+        return precioCompra;
+    }
+
+    public void setPrecioCompra(DecimalFormat precioCompra) {
+        this.precioCompra = precioCompra;
+    }
+
+    public DecimalFormat getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(DecimalFormat precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public int getIdTipoIva() {
+        return idTipoIva;
+    }
+
+    public void setIdTipoIva(int idTipoIva) {
+        this.idTipoIva = idTipoIva;
+    }
+
+    public boolean isHabilitado() {
+        return habilitado;
+    }
+
+    public void setHabilitado(boolean habilitado) {
+        this.habilitado = habilitado;
+    }
+
+    public BigInteger getIdTransaccion() {
+        return idTransaccion;
+    }
+
+    public void setIdTransaccion(BigInteger idTransaccion) {
+        this.idTransaccion = idTransaccion;
+    }
+    
+    
+    
+    
 }
--- Prototipos/presentacionSpringV1/src/java/SAF/VO/Stock/TipoIvaVO.java
+++ Prototipos/presentacionSpringV1/src/java/SAF/VO/Stock/TipoIvaVO.java
@@ -5,11 +5,39 @@
  */
 package SAF.VO.Stock;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
 /**
  *
  * @author majo
  */
 public class TipoIvaVO {
     String descripcion;
-    //porcentaje
+    DecimalFormat porcentaje;
+
+    public TipoIvaVO() {
+        DecimalFormatSymbols simbolos = new DecimalFormatSymbols();
+        simbolos.setDecimalSeparator('.');
+        porcentaje = new DecimalFormat("###.##",simbolos);
+
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public DecimalFormat getPorcentaje() {
+        return porcentaje;
+    }
+
+    public void setPorcentaje(DecimalFormat porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+    
+    
 }
